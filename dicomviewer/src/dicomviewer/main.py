4d5057924f982b7f3a06b53d6baebe3b7a0f8c43
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-01-28 13:46:19 +0100

    Implemented simple main.py

---
+++ dicomviewer/src/dicomviewer/main.py
@@ -0,0 +1,6 @@
+def main():
+    print('Running DICOM viewer...')
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/installdependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mamba install -c conda-forge \
+    pyside6 \
+    numpy \
+    pandas \
+    pydicom \
+    pillow \
+    poetry \
+    matplotlib \
+    SimpleITK
