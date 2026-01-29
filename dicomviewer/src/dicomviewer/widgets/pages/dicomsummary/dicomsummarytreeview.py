62b8a62cc89c43e6199da476628404296dbc2730
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-01-29 14:23:18 +0100

    Saving work

--- dicomviewer/src/dicomviewer/widgets/pages/dicomsummary/dicomsummarytreeview.py
+++ dicomviewer/src/dicomviewer/widgets/pages/dicomsummary/dicomsummarytreeview.py
@@ -20,6 +20,8 @@ class DicomSummaryTreeView(QTreeView):
         self.setSortingEnabled(True)
         self.expandToDepth(0)
         self.setUniformRowHeights(True)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
     def series_dict(self):
         return self._series_dict
@@ -28,40 +30,60 @@ class DicomSummaryTreeView(QTreeView):
         self._series_dict = series_dict
         self._model = self.build_model_from_series_dict(self._series_dict, search_pattern)
         self.setModel(self._model)
-        self.header().setStretchLastSection(False)
-        self.header().setSectionResizeMode(0, QHeaderView.Stretch)
-        self.header().setSectionResizeMode(1, QHeaderView.Stretch)
-        self.header().setSectionResizeMode(2, QHeaderView.ResizeToContents)
-        self.header().setSectionResizeMode(3, QHeaderView.ResizeToContents)
-        self.setColumnWidth(2, 80)
-        self.setColumnWidth(3, 40)
+        for i in range(10):
+            self.header().setSectionResizeMode(i, QHeaderView.ResizeToContents)
 
     def filter_model_with_search_pattern(self, search_pattern):
         if self.series_dict():
             self.update_model(self.series_dict(), search_pattern)
 
     def build_model_from_series_dict(self, series_dict, search_pattern=None):
+        header = ['description', 'patient_id', 'thickness', 'rows', 'cols', 'spacingx', 'spacingy', 'image_type', 'manufacturer', 'nr_files']
         model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(['description', 'thickness', 'patient_id', 'nr_files'])
+        model.setHorizontalHeaderLabels(header)
         for suid, series_info in series_dict.items():
             description = series_info.get('description', '')
             if search_pattern is None or (search_pattern is not None and self.matches(search_pattern, series_info)):
-                patient_id = series_info.get('patient_id', '')
+                patient_id = series_info.get('patient_id', '')                
+                thickness = series_info.get('thickness', 0)
+                rows = series_info.get('rows', 0)
+                cols = series_info.get('cols', 0)
+                spacingx = series_info.get('spacingx', 0.0)
+                spacingy = series_info.get('spacingy', 0.0)
+                image_type = series_info.get('image_type', '')
+                image_type = '/'.join(image_type)
+                manufacturer = series_info.get('manufacturer', '')
                 files = series_info.get('files', [])
                 nrfiles = len(files)
-                thickness = series_info.get('thickness', 0)
-                description_item = QStandardItem(description)
-                description_item.setData(suid, role=Qt.UserRole)
-                description_item.setFlags(description_item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
-                description_item.setCheckState(Qt.CheckState.Checked if self._all_selected else Qt.CheckState.Unchecked)
-                patient_item = QStandardItem(patient_id)
-                nrfiles_item = QStandardItem(str(nrfiles))
-                thickness_item = QStandardItem(str(thickness))
-                model.appendRow([description_item, thickness_item, patient_item, nrfiles_item])
+                row = self.create_series_row(suid, description, patient_id, thickness, rows, cols, spacingx, spacingy, image_type, manufacturer, nrfiles)
+                model.appendRow(row)
                 for f in files:
                     f_item = QStandardItem(f)
-                    description_item.appendRow([f_item, QStandardItem(''), QStandardItem('')])
+                    row[0].appendRow(self.create_file_row(f_item, n_empty=len(header) - 1))
         return model
+    
+    def create_series_row(self, suid, description, patient_id, thickness, rows, cols, spacingx, spacingy, image_type, manufacturer, nrfiles):
+        description_item = QStandardItem(description)
+        description_item.setData(suid, role=Qt.UserRole)
+        description_item.setFlags(description_item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+        description_item.setCheckState(Qt.CheckState.Checked if self._all_selected else Qt.CheckState.Unchecked)
+        return [
+            description_item,
+            QStandardItem(patient_id),
+            QStandardItem(str(thickness)),
+            QStandardItem(str(rows)),
+            QStandardItem(str(cols)),
+            QStandardItem(str(spacingx)),
+            QStandardItem(str(spacingy)),
+            QStandardItem(image_type),
+            QStandardItem(manufacturer),
+            QStandardItem(str(nrfiles)),
+        ]
+    
+    def create_file_row(self, item, n_empty):
+        row = [item]
+        for i in range(n_empty): row.append(QStandardItem(''))
+        return row
     
     def select_all(self, mode=True):
         if self._model:
